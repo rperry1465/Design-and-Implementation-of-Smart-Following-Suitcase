b1f8e402de25c15f3973dfdf7cfb1c10018a16ef
tkurth1 <tkurth1@emich.edu>
2026-03-03 16:20:10 -0500

    Update MAIN.ino

--- MAIN/MAIN.ino
+++ MAIN/MAIN.ino
@@ -1,9 +1,20 @@
+#include "Motor_Function.hpp"
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); // Serial for debugging
+
+  if(!AFMS.begin()){ //Check Shield Connection
+    Serial.println("Could not connect to Motorshield, reset program.");
+    while(1);
+  }
+
+  //Motor tuning (Comment out when done)
+  delay(2000);
+  Go_Forward(130,1000);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
